e866cd9d5b1b42904bf1c5c6792d3a6491e76436
IgorDoring <idoring@tutanota.com>
2023-05-26 22:00:13 -0300

    Params in urls

--- src/app/pages/sobre/sobre.component.ts
+++ src/app/pages/sobre/sobre.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-sobre',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./sobre.component.css']
 })
 export class SobreComponent {
+  constructor(private activatedRoute: ActivatedRoute) {
+  }
 
+  ngOnInit(): void{
+    this.activatedRoute.params.subscribe(
+      (res) => console.log(res),
+      (e) => console.log(e)
+  )
+
+    this.activatedRoute.queryParams.subscribe(
+      (res) => console.log(res),
+      (e) => console.log(e)
+    )
+  }
 }
