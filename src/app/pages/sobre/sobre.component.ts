61c77fdd0b4b8ef71693bd029404c3762516bc8f
IgorDoring <idoring@tutanota.com>
2023-05-26 22:09:28 -0300

    rerouting by TS

--- src/app/pages/sobre/sobre.component.ts
+++ src/app/pages/sobre/sobre.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-sobre',
@@ -7,18 +7,23 @@ import {ActivatedRoute} from "@angular/router";
   styleUrls: ['./sobre.component.css']
 })
 export class SobreComponent {
-  constructor(private activatedRoute: ActivatedRoute) {
+  constructor(private activatedRoute: ActivatedRoute,
+              private router: Router) {
   }
 
   ngOnInit(): void{
     this.activatedRoute.params.subscribe(
-      (res) => console.log(res),
+      (v) => console.log(v),
       (e) => console.log(e)
   )
 
     this.activatedRoute.queryParams.subscribe(
-      (res) => console.log(res),
+      (v) => console.log(v),
       (e) => console.log(e)
     )
+
+    setInterval(() => {
+      this.router.navigate(["404"])
+    }, 4000)
   }
 }
